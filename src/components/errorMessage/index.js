6437795a3ed024f7f6bd6116193324000578382b
trel1 <trel@netcraft.global>
2020-07-21 17:42:47 +0300

    random char, preloader, error

---
+++ src/components/errorMessage/index.js
@@ -0,0 +1,3 @@
+import errorMessage from './errorMessage'
+import ErrorMessage from './errorMessage'
+export default ErrorMessage
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spin-94t07dbj2h"><div class="ldio-dzb4xnfeqj">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
